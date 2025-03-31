6d63588bdf3185b7d28b177a6ed4817437950852
Berkay Arslan <arslanberkay18@gmail.com>
2025-03-31 10:25:22 +0300

    Yol akışı, araba hareketleri ve oyun sonlandırma kodları yazıldı.

--- TurboDriftTrafficDodge.UI/Form1.Designer.cs
+++ TurboDriftTrafficDodge.UI/Form1.Designer.cs
@@ -29,114 +29,31 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            pnlYarisAlani = new Panel();
-            pbArabam = new PictureBox();
-            pbAraba5 = new PictureBox();
-            pbAraba4 = new PictureBox();
-            pbAraba3 = new PictureBox();
-            pbAraba2 = new PictureBox();
-            pbAraba1 = new PictureBox();
             btnOyunuBaslat = new Button();
             tmrHareket = new System.Windows.Forms.Timer(components);
+            pnlYarisAlani = new Panel();
+            pbAraba3 = new PictureBox();
+            pbAraba2 = new PictureBox();
+            pbAraba4 = new PictureBox();
+            pbPatlama = new PictureBox();
+            pbArabam = new PictureBox();
+            pbAraba1 = new PictureBox();
+            pbAraba5 = new PictureBox();
+            pbYol = new PictureBox();
             pnlYarisAlani.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pbArabam).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pbAraba5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pbAraba4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbAraba4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbPatlama).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbArabam).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbAraba5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbYol).BeginInit();
             SuspendLayout();
-            // 
-            // pnlYarisAlani
-            // 
-            pnlYarisAlani.BackgroundImage = Properties.Resources.yol;
-            pnlYarisAlani.Controls.Add(pbArabam);
-            pnlYarisAlani.Controls.Add(pbAraba5);
-            pnlYarisAlani.Controls.Add(pbAraba4);
-            pnlYarisAlani.Controls.Add(pbAraba3);
-            pnlYarisAlani.Controls.Add(pbAraba2);
-            pnlYarisAlani.Controls.Add(pbAraba1);
-            pnlYarisAlani.Location = new Point(38, 0);
-            pnlYarisAlani.Margin = new Padding(4);
-            pnlYarisAlani.Name = "pnlYarisAlani";
-            pnlYarisAlani.Size = new Size(1656, 777);
-            pnlYarisAlani.TabIndex = 0;
-            // 
-            // pbArabam
-            // 
-            pbArabam.BackColor = Color.Transparent;
-            pbArabam.Image = Properties.Resources.araba9;
-            pbArabam.Location = new Point(741, 569);
-            pbArabam.Margin = new Padding(4);
-            pbArabam.Name = "pbArabam";
-            pbArabam.Size = new Size(95, 165);
-            pbArabam.SizeMode = PictureBoxSizeMode.Zoom;
-            pbArabam.TabIndex = 1;
-            pbArabam.TabStop = false;
-            // 
-            // pbAraba5
-            // 
-            pbAraba5.BackColor = Color.Transparent;
-            pbAraba5.Image = Properties.Resources.araba7;
-            pbAraba5.Location = new Point(1456, 114);
-            pbAraba5.Margin = new Padding(4);
-            pbAraba5.Name = "pbAraba5";
-            pbAraba5.Size = new Size(95, 165);
-            pbAraba5.SizeMode = PictureBoxSizeMode.Zoom;
-            pbAraba5.TabIndex = 1;
-            pbAraba5.TabStop = false;
-            // 
-            // pbAraba4
-            // 
-            pbAraba4.BackColor = Color.Transparent;
-            pbAraba4.Image = Properties.Resources.araba3;
-            pbAraba4.Location = new Point(1145, 65);
-            pbAraba4.Margin = new Padding(4);
-            pbAraba4.Name = "pbAraba4";
-            pbAraba4.Size = new Size(95, 165);
-            pbAraba4.SizeMode = PictureBoxSizeMode.Zoom;
-            pbAraba4.TabIndex = 1;
-            pbAraba4.TabStop = false;
-            // 
-            // pbAraba3
-            // 
-            pbAraba3.BackColor = Color.Transparent;
-            pbAraba3.Image = Properties.Resources.araba8;
-            pbAraba3.Location = new Point(877, 52);
-            pbAraba3.Margin = new Padding(4);
-            pbAraba3.Name = "pbAraba3";
-            pbAraba3.Size = new Size(95, 165);
-            pbAraba3.SizeMode = PictureBoxSizeMode.Zoom;
-            pbAraba3.TabIndex = 1;
-            pbAraba3.TabStop = false;
-            // 
-            // pbAraba2
-            // 
-            pbAraba2.BackColor = Color.Transparent;
-            pbAraba2.Image = Properties.Resources.araba4;
-            pbAraba2.Location = new Point(337, 165);
-            pbAraba2.Margin = new Padding(4);
-            pbAraba2.Name = "pbAraba2";
-            pbAraba2.Size = new Size(95, 165);
-            pbAraba2.SizeMode = PictureBoxSizeMode.Zoom;
-            pbAraba2.TabIndex = 1;
-            pbAraba2.TabStop = false;
-            // 
-            // pbAraba1
-            // 
-            pbAraba1.BackColor = Color.Transparent;
-            pbAraba1.Image = Properties.Resources.araba5;
-            pbAraba1.Location = new Point(147, 36);
-            pbAraba1.Margin = new Padding(4);
-            pbAraba1.Name = "pbAraba1";
-            pbAraba1.Size = new Size(95, 165);
-            pbAraba1.SizeMode = PictureBoxSizeMode.Zoom;
-            pbAraba1.TabIndex = 1;
-            pbAraba1.TabStop = false;
             // 
             // btnOyunuBaslat
             // 
-            btnOyunuBaslat.Location = new Point(617, 785);
+            btnOyunuBaslat.Location = new Point(618, 831);
             btnOyunuBaslat.Margin = new Padding(4);
             btnOyunuBaslat.Name = "btnOyunuBaslat";
             btnOyunuBaslat.Size = new Size(518, 96);
@@ -147,40 +64,141 @@
             // 
             // tmrHareket
             // 
+            tmrHareket.Interval = 10;
             tmrHareket.Tick += tmrHareket_Tick;
+            // 
+            // pnlYarisAlani
+            // 
+            pnlYarisAlani.BackColor = SystemColors.ActiveCaptionText;
+            pnlYarisAlani.Controls.Add(pbAraba3);
+            pnlYarisAlani.Controls.Add(pbAraba2);
+            pnlYarisAlani.Controls.Add(pbAraba4);
+            pnlYarisAlani.Controls.Add(pbPatlama);
+            pnlYarisAlani.Controls.Add(pbArabam);
+            pnlYarisAlani.Controls.Add(pbAraba1);
+            pnlYarisAlani.Controls.Add(pbAraba5);
+            pnlYarisAlani.Controls.Add(pbYol);
+            pnlYarisAlani.Location = new Point(37, 12);
+            pnlYarisAlani.Name = "pnlYarisAlani";
+            pnlYarisAlani.Size = new Size(1656, 789);
+            pnlYarisAlani.TabIndex = 2;
+            // 
+            // pbAraba3
+            // 
+            pbAraba3.Image = Properties.Resources.araba7;
+            pbAraba3.Location = new Point(819, 122);
+            pbAraba3.Name = "pbAraba3";
+            pbAraba3.Size = new Size(65, 136);
+            pbAraba3.SizeMode = PictureBoxSizeMode.Zoom;
+            pbAraba3.TabIndex = 3;
+            pbAraba3.TabStop = false;
+            // 
+            // pbAraba2
+            // 
+            pbAraba2.Image = Properties.Resources.araba6;
+            pbAraba2.Location = new Point(428, 250);
+            pbAraba2.Name = "pbAraba2";
+            pbAraba2.Size = new Size(84, 186);
+            pbAraba2.SizeMode = PictureBoxSizeMode.Zoom;
+            pbAraba2.TabIndex = 3;
+            pbAraba2.TabStop = false;
+            // 
+            // pbAraba4
+            // 
+            pbAraba4.Image = Properties.Resources.araba3;
+            pbAraba4.Location = new Point(1147, 381);
+            pbAraba4.Name = "pbAraba4";
+            pbAraba4.Size = new Size(65, 136);
+            pbAraba4.SizeMode = PictureBoxSizeMode.Zoom;
+            pbAraba4.TabIndex = 3;
+            pbAraba4.TabStop = false;
+            // 
+            // pbPatlama
+            // 
+            pbPatlama.BackColor = Color.Transparent;
+            pbPatlama.Image = Properties.Resources.explosion;
+            pbPatlama.Location = new Point(819, 592);
+            pbPatlama.Name = "pbPatlama";
+            pbPatlama.Size = new Size(65, 53);
+            pbPatlama.SizeMode = PictureBoxSizeMode.Zoom;
+            pbPatlama.TabIndex = 3;
+            pbPatlama.TabStop = false;
+            // 
+            // pbArabam
+            // 
+            pbArabam.Image = Properties.Resources.araba8;
+            pbArabam.Location = new Point(819, 616);
+            pbArabam.Name = "pbArabam";
+            pbArabam.Size = new Size(65, 136);
+            pbArabam.SizeMode = PictureBoxSizeMode.Zoom;
+            pbArabam.TabIndex = 3;
+            pbArabam.TabStop = false;
+            // 
+            // pbAraba1
+            // 
+            pbAraba1.Image = Properties.Resources.araba4;
+            pbAraba1.Location = new Point(109, 33);
+            pbAraba1.Name = "pbAraba1";
+            pbAraba1.Size = new Size(65, 136);
+            pbAraba1.SizeMode = PictureBoxSizeMode.Zoom;
+            pbAraba1.TabIndex = 3;
+            pbAraba1.TabStop = false;
+            // 
+            // pbAraba5
+            // 
+            pbAraba5.Image = Properties.Resources.araba2;
+            pbAraba5.Location = new Point(1427, 64);
+            pbAraba5.Name = "pbAraba5";
+            pbAraba5.Size = new Size(65, 136);
+            pbAraba5.SizeMode = PictureBoxSizeMode.Zoom;
+            pbAraba5.TabIndex = 3;
+            pbAraba5.TabStop = false;
+            // 
+            // pbYol
+            // 
+            pbYol.Image = Properties.Resources.yol;
+            pbYol.Location = new Point(3, -463);
+            pbYol.Name = "pbYol";
+            pbYol.Size = new Size(1650, 1600);
+            pbYol.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbYol.TabIndex = 0;
+            pbYol.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 28F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1741, 911);
-            Controls.Add(btnOyunuBaslat);
+            ClientSize = new Size(1741, 940);
             Controls.Add(pnlYarisAlani);
+            Controls.Add(btnOyunuBaslat);
             Font = new Font("Segoe UI", 12F);
             Margin = new Padding(4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             pnlYarisAlani.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pbArabam).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pbAraba5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pbAraba4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbAraba4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbPatlama).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbArabam).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbAraba1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbAraba5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbYol).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel pnlYarisAlani;
-        private PictureBox pbArabam;
-        private PictureBox pbAraba5;
-        private PictureBox pbAraba4;
-        private PictureBox pbAraba3;
-        private PictureBox pbAraba2;
-        private PictureBox pbAraba1;
         private Button btnOyunuBaslat;
         private System.Windows.Forms.Timer tmrHareket;
+        private Panel pnlYarisAlani;
+        private PictureBox pbYol;
+        private PictureBox pbAraba3;
+        private PictureBox pbAraba2;
+        private PictureBox pbAraba4;
+        private PictureBox pbArabam;
+        private PictureBox pbAraba1;
+        private PictureBox pbAraba5;
+        private PictureBox pbPatlama;
     }
 }
--- TurboDriftTrafficDodge.UI/Form1.cs
+++ TurboDriftTrafficDodge.UI/Form1.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+
 namespace TurboDriftTrafficDodge.UI
 {
     public partial class Form1 : Form
@@ -7,9 +9,15 @@ namespace TurboDriftTrafficDodge.UI
             InitializeComponent();
         }
 
+        Random rnd = new Random();
+
+        int kazanilanPuan = 0;
+        int arabaHareketHizi = 5;
+        int yolHizi = 5;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            pbPatlama.Visible = false;
         }
 
         private void btnOyunuBaslat_Click(object sender, EventArgs e)
@@ -24,11 +32,101 @@ namespace TurboDriftTrafficDodge.UI
 
         private void tmrHareket_Tick(object sender, EventArgs e)
         {
-            pbAraba1.Top += 5;
-            pbAraba2.Top += 5;
-            pbAraba3.Top += 5;
-            pbAraba4.Top += 5;
-            pbAraba5.Top += 5;
+            pbYol.Top += yolHizi;
+            if (pbYol.Top > pnlYarisAlani.Height)
+            {
+                pbYol.Top = pbYol.Height * (-1);
+            }
+
+            pbAraba1.Top += arabaHareketHizi;
+            pbAraba2.Top += arabaHareketHizi;
+            pbAraba3.Top += arabaHareketHizi;
+            pbAraba4.Top += arabaHareketHizi;
+            pbAraba5.Top += arabaHareketHizi;
+
+            if (pbAraba1.Top > pnlYarisAlani.Height)
+            {
+                ArabaYenile(pbAraba1);
+            }
+            if (pbAraba2.Top > pnlYarisAlani.Height)
+            {
+                ArabaYenile(pbAraba2);
+            }
+            if (pbAraba3.Top > pnlYarisAlani.Height)
+            {
+                ArabaYenile(pbAraba3);
+            }
+            if (pbAraba4.Top > pnlYarisAlani.Height)
+            {
+                ArabaYenile(pbAraba4);
+            }
+            if (pbAraba5.Top > pnlYarisAlani.Height)
+            {
+                ArabaYenile(pbAraba5);
+            }
+
+            OyunuBitir();
         }
+
+        /// <summary>
+        /// Yol aktýkça giden arabalar yerine gelecek olan arabanýn konum ve tipini belirleyen metod
+        /// </summary>
+        /// <param name="pbAraba"></param>
+        private void ArabaYenile(PictureBox pbAraba)
+        {
+            int rasgeleAraba = rnd.Next(1, 10);
+            int rasgeleKonum = rnd.Next(1, 3);
+
+            pbAraba.Top = rnd.Next(0, 150) * (-1);
+
+            if (rasgeleKonum == 1)
+            {
+                pbAraba.Left += rnd.Next(0, 50);
+            }
+            else
+            {
+                pbAraba.Left -= rnd.Next(0, 50);
+            }
+
+            switch (rasgeleAraba)
+            {
+                case 1:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba2;
+                    break;
+                case 2:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba3;
+                    break;
+                case 3:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba4;
+                    break;
+                case 4:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba5;
+                    break;
+                case 5:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba6;
+                    break;
+                case 6:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba7;
+                    break;
+                case 7:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba8;
+                    break;
+                case 8:
+                    pbAraba.Image = TurboDriftTrafficDodge.UI.Properties.Resources.araba9;
+                    break;
+            }
+        }
+
+        private void OyunuBitir()
+        {
+            //Eðer benim arabam baþka bir araba yada engel ile ayný konuma girerse(çarparsa) oyun sonlanýr.
+            if ((pbArabam.Bounds.IntersectsWith(pbAraba1.Bounds) || pbArabam.Bounds.IntersectsWith(pbAraba2.Bounds) || pbArabam.Bounds.IntersectsWith(pbAraba3.Bounds) || pbArabam.Bounds.IntersectsWith(pbAraba4.Bounds) || pbArabam.Bounds.IntersectsWith(pbAraba5.Bounds)))
+            {
+                pbPatlama.Visible = true;
+                tmrHareket.Stop();
+            }
+        }
+
+
     }
 }
